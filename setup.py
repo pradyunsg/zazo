b8fcaf6b5247bf3e5b9422a4ce8a4708174afa63
Pradyun S. Gedam <pradyunsg@gmail.com>
2016-12-28 15:24:57 +0530

    Fix Travis CI build

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+
 # Keep README.md ASCII-only
 long_description = read('README.md')
 
---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+
+
+def pytest_collection_modifyitems(items):
+    """Marks the tests as unit or integration depending on which folder they
+    live in
+    """
+    for item in items:
+        # We don't want to touch DoctestTextfile and others.
+        if not hasattr(item, 'module'):
+            continue
+
+        # Get path of the file
+        module_path = os.path.relpath(
+            item.module.__file__,
+            os.path.commonprefix([__file__, item.module.__file__]),
+        )
+
+        # Mark the type of test
+        module_root_dir = module_path.split(os.pathsep)[0]
+        if module_root_dir.startswith("integration"):
+            item.add_marker(pytest.mark.integration)
+        elif module_root_dir.startswith("unit"):
+            item.add_marker(pytest.mark.unit)
+        else:
+            raise RuntimeError(
+                "Unknown test type (filename = {0}, root_dir = {1})".format(
+                    module_path, module_root_dir
+                )
+            )
