e1f5f15c9e58391a3d9bdc57f747679b84881537
Pradyun Gedam <pradyunsg@gmail.com>
2017-11-01 19:59:03 +0530

    [tests, yaml] now better no-op tests

--- tests/lib/yaml_fixtures.py
+++ tests/lib/yaml_fixtures.py
@@ -11,34 +11,33 @@ class YAMLException(Exception):
     """
 
 
-class DoesNotMatch(YAMLException):
-    """Raised when the result does not match the expected value
-    """
-
-
 class YamlFixtureFile(pytest.File):
 
+    def _compose_tests(self, data):
+        return []
+
     def collect(self):
-        data = yaml.safe_load(self.fspath.open())
-        print(data)
+        with self.fspath.open() as f:
+            data = yaml.safe_load(f)
+        yield from self._compose_tests(data)
 
 
-class YamlItem(pytest.Item):
-
-    def __init__(self, name, parent, spec):
-        # type: (str, Any, Any) -> None
-        super(YamlItem, self).__init__(name, parent)
-        self.spec = spec
-
-    def runtest(self):
-        # type: () -> None
-        raise DoesNotMatch()
-
-    # Called when the tests fail
-    def repr_failure(self, excinfo):
-        # type: (Any) -> str
-        if isinstance(excinfo.value, DoesNotMatch):
-            return "Test failed."
-
-    def reportinfo(self):
-        return self.fspath, 0, "yaml-test: %s" % self.name
+# class YamlItem(pytest.Item):
+#
+#     def __init__(self, name, parent, spec):
+#         # type: (str, Any, Any) -> None
+#         super(YamlItem, self).__init__(name, parent)
+#         self.spec = spec
+#
+#     def runtest(self):
+#         # type: () -> None
+#         raise DoesNotMatch()
+#
+#     # Called when the tests fail
+#     def repr_failure(self, excinfo):
+#         # type: (Any) -> str
+#         if isinstance(excinfo.value, DoesNotMatch):
+#             return "Test failed."
+#
+#     def reportinfo(self):
+#         return self.fspath, 0, "yaml-test: %s" % self.name
