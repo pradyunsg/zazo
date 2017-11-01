761348029b0261476713bf8b40c16f9207187efb
Pradyun Gedam <pradyunsg@gmail.com>
2017-11-01 22:58:37 +0530

    [misc] Empty __init__.py

--- src/zazo/__init__.py
+++ src/zazo/__init__.py
@@ -1,27 +1,4 @@
-"""A Dependency Resolver written in Python
+"""
 """
 
 __version__ = "0.1.0.dev0"
-
-# Exit codes
-SUCCESS = 0
-if False:
-    from typing import Text
-
-
-def get_message():
-    # type: () -> Text
-    """Returns a message that would be printed by main()
-    """
-    return "Hello World!"
-
-
-def main():
-    # type: () -> int
-    print(get_message())
-    return SUCCESS
-
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(main())
