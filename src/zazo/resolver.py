c3f4c7786a7b9a36f26f04a44080a12eb263a59a
Pradyun Gedam <pradyunsg@gmail.com>
2017-11-05 15:07:23 +0530

    [resolver] current_req -> req

--- src/zazo/resolver.py
+++ src/zazo/resolver.py
@@ -39,43 +39,41 @@ class BacktrackingResolver(object):
         # type: (List[Requirement], Graph) -> Graph
         if not requirements:
             return graph
-        current_req = requirements[0]
+        req = requirements[0]
         requirements = requirements[1:]
 
-        logger.debug("will attempt to satisfy: %s", current_req)
-        if current_req.name in graph:
+        logger.debug("will attempt to satisfy: %s", req)
+        if req.name in graph:
             # We have seen this requirement; check if we satisfy it already
-            existing_candidate = graph[current_req.name]
+            existing_candidate = graph[req.name]
             logger.debug("  already selected: %s", existing_candidate)
-            if not existing_candidate.matches(current_req):
+            if not existing_candidate.matches(req):
                 logger.debug("    does not match requirement")
-                raise CannotSatisfy(current_req, existing_candidate)
+                raise CannotSatisfy(req, existing_candidate)
             else:
                 logger.debug("    does match requirement")
                 # Proceed to the next requirement
                 return self._resolve(requirements, graph)
         else:
-            logger.debug("  not selected yet: %s", current_req.name)
-            candidates = self.provider.get_all_candidates(current_req)
-            logger.debug(
-                "  candidate count: %d", len(candidates), current_req.name
-            )
+            logger.debug("  not selected yet: %s", req.name)
+            candidates = self.provider.get_all_candidates(req)
+            logger.debug("  candidate count: %d", len(candidates), req.name)
             for candidate in self.provider.order_candidates(candidates):
-                assert candidate.matches(current_req), (
+                assert candidate.matches(req), (
                     "candidate does not match requirement it was guaranteed "
                     "to match"
                 )
                 logger.debug("  selecting: %s", candidate)
 
-                graph[current_req.name] = candidate
+                graph[req.name] = candidate
                 dependencies = self.provider.fetch_dependencies(candidate)
                 try:
                     return self._resolve(
                         dependencies + requirements, graph.copy()
                     )
                 except CannotSatisfy:
-                    assert graph[current_req.name] == candidate
-                    del graph[current_req.name]
+                    assert graph[req.name] == candidate
+                    del graph[req.name]
 
             # If we are here, we could not satisfy the given requirements
-            raise CannotSatisfy([current_req] + requirements, graph.copy())
+            raise CannotSatisfy([req] + requirements, graph.copy())
