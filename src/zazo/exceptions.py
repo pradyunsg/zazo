62cf6ef133f93e91d1cb1c2e3d328b1baeae308b
Pradyun Gedam <pradyunsg@gmail.com>
2017-11-12 15:24:40 +0530

    [exceptions] Remove redundant exceptions

--- src/zazo/exceptions.py
+++ src/zazo/exceptions.py
@@ -10,18 +10,3 @@ class ZazoError(Exception):
 class CannotSatisfy(ZazoError):
     """A requested candidate cannot be satisfied
     """
-
-
-class GraphError(ZazoError):
-    """Base of all Graph related exceptions
-    """
-
-
-class DuplicateVertexError(GraphError):
-    """A vertex cannot be duplicated
-    """
-
-
-class NonExistentVertexError(ZazoError):
-    """A vertex does not exist in class
-    """
