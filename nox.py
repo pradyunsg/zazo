f11d040cb2dfaa686044bcf335fff317bf8558fc
Pradyun Gedam <pradyunsg@gmail.com>
2018-05-12 12:44:11 +0530

    Rework Travis CI configuration
    HACK: Install pip on Py2/PyPy2 and install flit in it
     -> Because, Travis doesn't provide it in Py2/PyPy envs
    HACK: Build a wheel using flit and install it
     -> flit only supports Py3 (for supporting Py2)
    BODGE: Change directory into "tests", before running tests
     -> CWD is on sys.path; we want to be testing what's installed

--- nox.py
+++ nox.py
@@ -1,4 +1,6 @@
 import functools
+import glob
+import subprocess
 
 import nox
 
@@ -7,7 +9,6 @@ LINT_ITEMS = "nox.py", "docs/source/conf.py", "zazo", "tests"
 
 @nox.session
 def docs(session):
-    session.interpreter = "python3.6"
     session.install("-r", "tools/reqs/docs.txt")
 
     session.run("sphinx-build", "-n", "-b", "html", "docs/source", "docs/build")
@@ -15,7 +16,6 @@ def docs(session):
 
 @nox.session
 def packaging(session):
-    session.interpreter = "python3.6"
     session.install("-r", "tools/reqs/packaging.txt")
 
     session.run("flit", "build")
@@ -29,7 +29,7 @@ def lint_session(func):
             files = session.posargs
         else:
             files = LINT_ITEMS
-        session.install("-r", "tools/reqs/lint.txt")
+        session.install("--pre", "-r", "tools/reqs/lint.txt")
 
         return func(session, files)
 
@@ -57,11 +57,34 @@ def lint_format(session, files):
 @nox.session
 @nox.parametrize("python_version", ["2.7", "3.4", "3.5", "3.6", "3.7", "pypy", "pypy3"])
 def test(session, python_version):
+    # Set the interpreter
     if python_version.startswith("pypy"):
         session.interpreter = python_version
     else:
         session.interpreter = "python" + python_version
 
+    # Build the package.
+    # THIS IS A HACK
+    #   Working around all kinds of weird nox + flit + Travis CI behavior.
+    #   We're building a wheel here and installing it with session.install since
+    #   nox is declarative but we need to run the build command before executing
+    #   code.
+    def my_run(*args):
+        print("run > " + " ".join(args))
+        try:
+            subprocess.check_call(args)
+        except subprocess.CalledProcessError:
+            session.error("Command failed.")
+
+    my_run("python3", "-m", "flit", "build")
+    files = glob.glob("./dist/*.whl")
+    if not files:
+        session.error("Could not find any built wheels.")
+
+    # Install the package and test dependencies.
+    session.install(*files)
     session.install("-r", "tools/reqs/test.txt")
 
+    # Run the tests
+    session.cd("tests")  # we change directory to avoid the cwd ambiguity
     session.run("pytest", *session.posargs)
