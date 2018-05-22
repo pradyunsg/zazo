2d841bd7dcd7a7b564d8749b7faa9c9634f0dc55
Pradyun Gedam <pradyunsg@gmail.com>
2018-05-22 15:36:14 +0530

    Format Error message in the exception

--- tests/lib/yaml/exceptions.py
+++ tests/lib/yaml/exceptions.py
@@ -1,3 +1,19 @@
 class YAMLException(Exception):
     """Base for the exception hierarchy of this module
     """
+
+    def __str__(self):
+        # Format a reason
+        if not self.args:
+            message = "unknown"
+        elif len(self.args) == 1:
+            message = self.args[0]
+        else:
+            try:
+                message = self.args[0].format(*self.args[1:])
+            except Exception as error:
+                message = "Unable to format message: {}\n{}".format(
+                    self.args, error
+                )
+        # Print the reason
+        return "YAML is malformed -- reason: {}".format(message)
