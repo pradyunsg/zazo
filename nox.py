6abf4a5a9b1579027d1d85f332bbeb4be9b1ecb1
Pradyun Gedam <pradyunsg@gmail.com>
2018-08-15 13:10:16 +0530

    Switch to the newer nox and simplify configuration

--- nox.py
+++ nox.py
@@ -58,29 +58,11 @@ def format(session, files):
     session.run("isort", "--recursive", *files)
 
 
-@nox.session
-@nox.parametrize("python_version", ["2.7", "3.4", "3.5", "3.6", "3.7", "pypy", "pypy3"])
-def test(session, python_version):
-    # Set the interpreter
-    if python_version.startswith("pypy"):
-        session.interpreter = python_version
-    else:
-        session.interpreter = "python" + python_version
+@nox.session(python=["2.7", "3.4", "3.5", "3.6", "3.7", "pypy", "pypy3"])
+def test(session):
+    session.install("flit")
+    session.run("python3", "-m", "flit", "build")
 
-    # Build the package.
-    # THIS IS A HACK
-    #   Working around all kinds of weird nox + flit + Travis CI behavior.
-    #   We're building a wheel here and installing it with session.install since
-    #   nox is declarative but we need to run the build command before executing
-    #   code.
-    def my_run(*args):
-        print("run > " + " ".join(args))
-        try:
-            subprocess.check_call(args)
-        except subprocess.CalledProcessError:
-            session.error("Command failed.")
-
-    my_run("python3", "-m", "flit", "build")
     files = glob.glob("./dist/*.whl")
     if not files:
         session.error("Could not find any built wheels.")
