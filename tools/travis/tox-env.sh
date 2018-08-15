3caf9d10fa1105405c278b4e0e312a63c85109f8
Pradyun Gedam <pradyunsg@gmail.com>
2018-08-15 15:18:06 +0530

    Prepare Travis for the changes made

---
+++ tools/travis/tox-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+set -e
+
+echo "Determining correct TOXENV..."
+if [[ -z "$TOXENV" ]]; then
+    if [[ ${TRAVIS_PYTHON_VERSION} == pypy* ]]; then
+        export TOXENV=${TRAVIS_PYTHON_VERSION}
+    else
+        # We use the syntax ${string:index:length} to make 2.7 -> py27
+        _major=${TRAVIS_PYTHON_VERSION:0:1}
+        _minor=${TRAVIS_PYTHON_VERSION:2:1}
+        export TOXENV="py${_major}${_minor}"
+    fi
+fi
+echo "TOXENV=${TOXENV}"
